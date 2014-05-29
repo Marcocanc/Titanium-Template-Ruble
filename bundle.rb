11779872e5f62acd51e95b46cb76a23f99b11942
Marco Cancellieri <marco@clever-apps.com>
2014-05-29 18:36:08 +0200

    first commit

---
+++ bundle.rb
@@ -0,0 +1,14 @@
+require 'ruble'
+
+bundle do |bundle|
+  bundle.display_name = 'Basic App Template'
+  bundle.author = 'Marco Cancellieri'
+  bundle.copyright = <<END
+(c) Copyright 2014 Marco Cancellieri. Distributed under MIT license.
+END
+
+  bundle.description = <<END
+A Project Template for Titanium mobile projects.
+END
+
+end
---
+++ templates/templates.rb
@@ -0,0 +1,10 @@
+require 'ruble'
+
+
+project_template "Basic App Template" do |t|
+  t.type = :titanium_mobile
+  t.location = "https://github.com/Marcocanc/Titanium-App-Template.git"
+  t.description = "Marco's Basic App Template."
+  t.replace_parameters = true
+end
+
